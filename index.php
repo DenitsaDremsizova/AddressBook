222eb736d16ae45f98a2b755703945e2af008767
DenitsaDremsizova <denitsa.dremsizova@gmail.com>
2017-03-19 10:07:05 +0200

    resolved conflicts

--- index.php
+++ index.php
@@ -44,9 +44,9 @@ for($index=0; $index < count($arr); $index++){
                         <?php
                         foreach ($arr as $contact) {
                             if($contact['FirstElementWithLetter']){
-                                echo "<li id='index-$contact[2]'><a href='./profile.php?contact_id=$contact[0]'><img src='$contact[1]' class='contactPic'><span class='name'>$contact[0]</span></a><img src='./assets/images/delete-button.png' class='delete-button'></li>";
+                                echo "<li id='index-$contact[3]'><a href='./profile.php?contact_id=$contact[0]'><img src='$contact[2]' class='contactPic'><span class='name'>$contact[1]</span></a><img src='./assets/images/delete-button.png' class='delete-button'></li>";
                             } else {
-                            echo "<li><a href='./profile.php?contact_id=$contact[0]'><img src='$contact[1]' class='contactPic'><span class='name'>$contact[0]</span></a><img src='./assets/images/delete-button.png' class='delete-button'></li>";
+                            echo "<li><a href='./profile.php?contact_id=$contact[0]'><img src='$contact[2]' class='contactPic'><span class='name'>$contact[1]</span></a><img src='./assets/images/delete-button.png' class='delete-button'></li>";
                                 
                             }
                         }
